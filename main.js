65bfa46a1749be0f82e62e263b8125beb950710f
nazliisgenderova <127404424+nazliisgenderova@users.noreply.github.com>
2023-04-11 19:51:14 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+// var num;
+// if(num%30==0)
+// {
+//     allert('eded hem 3e hem 10a bolunur')
+// }
+// else{
+//     alert('yalnish deyer')
+// }
+
+// var point = 98;
+// if(point>91 && point<100){
+//    alert('A')
+// }
+// else if(point>81 && point<90){
+//    alert('B')
+// }
+// else if(point>71 && point<80){
+//    alert('C')
+// }
+// else if(point>61 && point<70){
+//    alert('D')
+// }
+// else if(point>51 && point<60){
+//    alert('E')
+// }
+// else if(point>0 && point<50){
+//    alert('F')
+// }
+// else if(point>100 && point<0){
+//     alert('Kesildi')
+// }
+
+// var x=4,y=6;
+// if(x%2==0 && y%2==0){
+//     alert(x+y)
+// }
+// else{
+//     alert('false')
+// }
+
+// var num = 98
+// if(num>9 && num<100){
+//     var teklik=num%10
+//     var onluq=(num-teklik)/10
+
+//     if(teklik > onluq){
+//         alert("teklik boyukdur")
+//     }
+//     else{
+//         alert("onluq boyukdur")
+//     }
+// }
